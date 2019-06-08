39ad1ab1959f5664a6b5241dccf698896427f07f
danielamuniz367 <danielamuniz367@gmail.com>
2019-06-08 17:23:54 -0500

    added getHouses function here

--- houses.js
+++ houses.js
@@ -1,58 +1,70 @@
-module.exports = function(){
-    var express = require('express');
-    var router = express.Router();
+module.exports = function () {
+  var express = require('express');
+  var router = express.Router();
 
-    function serveHouses(req, res){
-        console.log("You asked me for some houses?")
-        var query = 'SELECT id, name FROM houses'
-        var mysql = req.app.get('mysql');
-        var context = {};
 
-        function handleRenderingOfHouses(error, results, fields){
-          console.log(error)
-          console.log(results)
-          console.log(fields)
-          //take the results of that query and store ti inside context
-          context.houses = results;
-          //pass it to handlebars to put inside a file
-          res.render('houses', context)
-        }
-        //execute the sql query
-        mysql.pool.query(query, handleRenderingOfHouses)
-
-        //res.send('Here you go!');
-    }
-
-    function serveOneHouse(chicken, steak) {
-      console.log(chicken.params.fancyId);
-      console.log(chicken.params);
-      fancyId = chicken.params.fancyId
-
-      var queryString = "SELECT id, name FROM houses WHERE id = ?"
-
-      var mysql = steak.app.get('mysql')
-      var context = {};
-
-      function handleRenderingOfOneHouse(error, results, fields){
-          console.log("results are " + results)
-          context.planet = results[0]
-          console.log(context)
-
-          if(error){
-            console.log(error)
-            steak.write(error)
-            steak.end();
-          }else{
-            steak.render('serverHouse',context);
-          }
+  function getHouses(res, mysql, context, complete) {
+    mysql.pool.query("SELECT houses.id as id, name FROM houses", function (error, results, fields) {
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
       }
-      //execute the query
-      var queryString = mysql.pool.query(queryString, fancyId, handleRenderingOfOneHouse);
+      context.houses = results;
+      complete();
+    });
+  }
 
-      //steak.send("Here's a good tasty well done steak");
+  function serveHouses(req, res) {
+    console.log("You asked me for some houses?")
+    var query = "SELECT id, name FROM houses"
+    var mysql = req.app.get('mysql');
+    var context = {};
+
+    function handleRenderingOfHouses(error, results, fields) {
+      console.log(error)
+      console.log(results)
+      console.log(fields)
+      //take the results of that query and store ti inside context
+      context.houses = results;
+      //pass it to handlebars to put inside a file
+      res.render('houses', context)
     }
+    //execute the sql query
+    mysql.pool.query(query, handleRenderingOfHouses)
 
-    router.get('/', serveHouses);
-    router.get('/:fancyId', serveOneHouse);
-    return router;
+    //res.send('Here you go!');
+  }
+
+  function serveOneHouse(chicken, steak) {
+    console.log(chicken.params.fancyId);
+    console.log(chicken.params);
+    fancyId = chicken.params.fancyId
+
+    var queryString = "SELECT id, name FROM houses WHERE id = ?"
+
+    var mysql = steak.app.get('mysql')
+    var context = {};
+
+    function handleRenderingOfOneHouse(error, results, fields) {
+      console.log("results are " + results)
+      context.planet = results[0]
+      console.log(context)
+
+      if (error) {
+        console.log(error)
+        steak.write(error)
+        steak.end();
+      } else {
+        steak.render('serverHouse', context);
+      }
+    }
+    //execute the query
+    var queryString = mysql.pool.query(queryString, fancyId, handleRenderingOfOneHouse);
+
+    //steak.send("Here's a good tasty well done steak");
+  }
+
+  router.get('/', serveHouses);
+  router.get('/:fancyId', serveOneHouse);
+  return router;
 }();
