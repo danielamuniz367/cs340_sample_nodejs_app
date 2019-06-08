67e08ea1cb99fc821e42e543f86d564e7f98f049
danielamuniz367 <danielamuniz367@gmail.com>
2019-06-08 18:09:00 -0500

    trying again

--- houses.js
+++ houses.js
@@ -2,30 +2,28 @@ module.exports = function () {
   var express = require('express');
   var router = express.Router();
 
-
-  function getHouses(res, mysql, context, complete) {
-    mysql.pool.query("SELECT houses.id as id, name FROM houses", function (error, results, fields) {
-      if (error) {
-        res.write(JSON.stringify(error));
-        res.end();
-      }
-      context.houses = results;
-      complete();
-    });
-  }
-
-  router.get('/', function (req, res) {
-    var callbackCount = 0;
-    var context = {};
+  function servePlanets(req, res){
+    console.log("You asked me for some planets?")
+    var query = 'SELECT id, name FROM houses';
     var mysql = req.app.get('mysql');
-    getHouses(res, mysql, context, complete);
+    var context = {};
 
-    function complete() {
-      callbackCount++;
-      if (callbackCount >= 2) {
-        res.render('houses', context);
-      }
+    function handleRenderingOfPlanets(error, results, fields){
+      console.log(error)
+      console.log(results)
+      console.log(fields)
+      //take the results of that query and store ti inside context
+      context.planets = results;
+      //pass it to handlebars to put inside a file
+      res.render('houses', context)
     }
-  });
+    //execute the sql query
+    mysql.pool.query(query, handleRenderingOfPlanets)
+
+    //res.send('Here you go!');
+}
+
+router.get('/', servePlanets);
+
   return router;
 }();
