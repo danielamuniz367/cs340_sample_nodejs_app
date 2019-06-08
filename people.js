a83db0aafa43ed4cc22a49d4ac9621ed8821266a
danielamuniz367 <danielamuniz367@gmail.com>
2019-06-08 14:35:19 -0500

    changed to houses

--- people.js
+++ people.js
@@ -8,7 +8,7 @@ module.exports = function(){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.planets  = results;
+            context.houses = results;
             complete();
         });
     }
