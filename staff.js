750810d4d0a7b39ad9c9b0483941c2b55b931830
danielamuniz367 <danielamuniz367@gmail.com>
2019-06-09 10:27:32 -0500

    seeing if this changes

--- staff.js
+++ staff.js
@@ -82,7 +82,7 @@ module.exports = function(){
         function complete(){
             callbackCount++;
             if(callbackCount >= 2){
-                res.render('people', context);
+                res.render('staff', context);
             }
 
         }
@@ -99,7 +99,7 @@ module.exports = function(){
         function complete(){
             callbackCount++;
             if(callbackCount >= 2){
-                res.render('people', context);
+                res.render('staff', context);
             }
 
         }
