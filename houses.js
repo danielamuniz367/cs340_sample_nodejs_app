a41f3d408ce14d01fbbd43ea470cba544c264e73
danielamuniz367 <danielamuniz367@gmail.com>
2019-06-12 18:55:21 -0500

    testing this out

--- houses.js
+++ houses.js
@@ -1,87 +1,78 @@
-// module.exports = function () {
-//   var express = require('express');
-//   var router = express.Router();
-
-//   function servePlanets(req, res) {
-//     alert('IS THIS WORKING?');  
-//     console.log("You asked me for some planets?")
-//     var query = 'SELECT id, name FROM houses';
-//     var mysql = req.app.get('mysql');
-//     var context = {};
-
-//     function handleRenderingOfPlanets(error, results, fields) {
-//       console.log(error)
-//       console.log(results)
-//       console.log(fields)
-//       //take the results of that query and store ti inside context
-//       context.houses = results;
-//       //pass it to handlebars to put inside a file
-//       res.render('houses', context)
-//     }
-//     //execute the sql query
-//     mysql.pool.query(query, handleRenderingOfPlanets)
-
-//     //res.send('Here you go!');
-//   }
-
-//   router.get('/', servePlanets);
-//   return router;
-// }();
-module.exports = function(){
+module.exports = function () {
   var express = require('express');
   var router = express.Router();
 
-  function servePlanets(req, res){
-      console.log("You asked me for some planets?")
-      var query = 'SELECT planet_id, name, population FROM bsg_planets';
-      var mysql = req.app.get('mysql');
-      var context = {};
-
-      function handleRenderingOfPlanets(error, results, fields){
-        console.log(error)
-        console.log(results)
-        console.log(fields)
-        //take the results of that query and store ti inside context
-        context.planets = results;
-        //pass it to handlebars to put inside a file
-        res.render('houses', context)
+  function getHouses(res, mysql, context, complete) {
+    mysql.pool.query("SELECT houses.id as id, name FROM houses", function (error, results, fields) {
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
       }
-      //execute the sql query
-      mysql.pool.query(query, handleRenderingOfPlanets)
-
-      //res.send('Here you go!');
+      context.houses = results;
+      complete();
+    });
   }
 
-  function serveOnePlanet(chicken, steak) {
-    console.log(chicken.params.fancyId);
-    console.log(chicken.params);
-    fancyId = chicken.params.fancyId
+  // function getPeoplebyHomeworld(req, res, mysql, context, complete){
+  //   var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.homeworld = ?";
+  //   console.log(req.params)
+  //   var inserts = [req.params.homeworld]
+  //   mysql.pool.query(query, inserts, function(error, results, fields){
+  //         if(error){
+  //             res.write(JSON.stringify(error));
+  //             res.end();
+  //         }
+  //         context.people = results;
+  //         complete();
+  //     });
+  // }
 
-    var queryString = "SELECT planet_id, name, population, language, capital FROM bsg_planets WHERE planet_id = ?"
+  // /* Find people whose fname starts with a given string in the req */
+  // function getPeopleWithNameLike(req, res, mysql, context, complete) {
+  //   //sanitize the input as well as include the % character
+  //    var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.fname LIKE " + mysql.pool.escape(req.params.s + '%');
+  //   console.log(query)
 
-    var mysql = steak.app.get('mysql')
+  //   mysql.pool.query(query, function(error, results, fields){
+  //         if(error){
+  //             res.write(JSON.stringify(error));
+  //             res.end();
+  //         }
+  //         context.people = results;
+  //         complete();
+  //     });
+  // }
+
+  // function getPerson(res, mysql, context, id, complete){
+  //     var sql = "SELECT character_id as id, fname, lname, homeworld, age FROM bsg_people WHERE character_id = ?";
+  //     var inserts = [id];
+  //     mysql.pool.query(sql, inserts, function(error, results, fields){
+  //         if(error){
+  //             res.write(JSON.stringify(error));
+  //             res.end();
+  //         }
+  //         context.person = results[0];
+  //         complete();
+  //     });
+  // }
+
+  /*Display all people. Requires web based javascript to delete users with AJAX*/
+
+  router.get('/', function (req, res) {
+    var callbackCount = 0;
     var context = {};
+    context.jsscripts = ["deleteperson.js", "filterpeople.js", "searchpeople.js"];
+    var mysql = req.app.get('mysql');
+    getStudents(res, mysql, context, complete);
 
-    function handleRenderingOfOnePlanet(error, results, fields){
-        console.log("results are " + results)
-        context.planet = results[0]
-        console.log(context)
+    function complete() {
+      callbackCount++;
+      if (callbackCount >= 1) {
+        res.render('houses', context);
+      }
 
-        if(error){
-          console.log(error)
-          steak.write(error)
-          steak.end();
-        }else{
-          steak.render('serverPlanet',context);
-        }
     }
-    //execute the query
-    var queryString = mysql.pool.query(queryString, fancyId, handleRenderingOfOnePlanet);
+  });
 
-    //steak.send("Here's a good tasty well done steak");
-  }
-
-  router.get('/', servePlanets);
-  router.get('/:fancyId', serveOnePlanet);
   return router;
 }();
--- main.js
+++ main.js
@@ -17,8 +17,9 @@ app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/people', require('./people.js'));
-app.use('/houses', require('./houses.js'));
 app.use('/', express.static('public'));
+
+
 
 app.use(function(req,res){
   res.status(404);
