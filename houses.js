29ed1d2a05a19583208f5a160d680651b9eb3728
danielamuniz367 <danielamuniz367@gmail.com>
2019-06-12 19:35:51 -0500

    trying this out again

--- houses.js
+++ houses.js
@@ -1,16 +1,30 @@
-module.exports = function () {
+module.exports = function(){
   var express = require('express');
   var router = express.Router();
 
-  function getHouses(res, mysql, context, complete) {
-    mysql.pool.query("SELECT houses.id as id, name FROM houses", function (error, results, fields) {
-      if (error) {
-        res.write(JSON.stringify(error));
-        res.end();
-      }
-      context.houses = results;
-      complete();
-    });
+  function getHouses(res, mysql, context, complete){
+      mysql.pool.query("SELECT houses.id as id, name FROM houses", function(error, results, fields){
+          if(error){
+              res.write(JSON.stringify(error));
+              res.end();
+          }
+          context.houses = results;
+          complete();
+      });
+  }
+
+  function getStudents(res, mysql, context, complete){
+      // mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields){
+      mysql.pool.query("SELECT students.id as id, fname, lname, type, class_year, house_id FROM students", function(error, results, fields){
+      // mysql.pool.query("SELECT students.id as id, fname, lname, students.type as type, class_year, houses.name as house_id FROM students INNER JOIN houses on house_id = houses.id", function(error, results, fields){
+
+          if(error){
+              res.write(JSON.stringify(error));
+              res.end();
+          }
+          context.people = results;
+          complete();
+      });
   }
 
   // function getPeoplebyHomeworld(req, res, mysql, context, complete){
@@ -58,19 +72,128 @@ module.exports = function () {
 
   /*Display all people. Requires web based javascript to delete users with AJAX*/
 
-  router.get('/', function (req, res) {
-    var callbackCount = 0;
-    var context = {};
-    var mysql = req.app.get('mysql');
-    getHouses(res, mysql, context, complete);
+  router.get('/', function(req, res){
+      var callbackCount = 0;
+      var context = {};
+      context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+      var mysql = req.app.get('mysql');
+      getStudents(res, mysql, context, complete);
+      getHouses(res, mysql, context, complete);
+      function complete(){
+          callbackCount++;
+          if(callbackCount >= 2){
+              res.render('people', context);
+          }
 
-    function complete() {
-      callbackCount++;
-      if (callbackCount >= 1) {
-        res.render('houses', context);
       }
+  });
 
-    }
+  /*Display all people from a given homeworld. Requires web based javascript to delete users with AJAX*/
+  router.get('/filter/:homeworld', function(req, res){
+      var callbackCount = 0;
+      var context = {};
+      context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+      var mysql = req.app.get('mysql');
+      getPeoplebyHomeworld(req,res, mysql, context, complete);
+      getHouses(res, mysql, context, complete);
+      function complete(){
+          callbackCount++;
+          if(callbackCount >= 2){
+              res.render('people', context);
+          }
+
+      }
+  });
+
+  /*Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX */
+  router.get('/search/:s', function(req, res){
+      var callbackCount = 0;
+      var context = {};
+      context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+      var mysql = req.app.get('mysql');
+      getPeopleWithNameLike(req, res, mysql, context, complete);
+      getHouses(res, mysql, context, complete);
+      function complete(){
+          callbackCount++;
+          if(callbackCount >= 2){
+              res.render('people', context);
+          }
+      }
+  });
+
+  /* Display one person for the specific purpose of updating people */
+
+  router.get('/:id', function(req, res){
+      callbackCount = 0;
+      var context = {};
+      context.jsscripts = ["selectedplanet.js", "updateperson.js"];
+      var mysql = req.app.get('mysql');
+      getPerson(res, mysql, context, req.params.id, complete);
+      getHouses(res, mysql, context, complete);
+      function complete(){
+          callbackCount++;
+          if(callbackCount >= 2){
+              res.render('update-person', context);
+          }
+
+      }
+  });
+
+  /* Adds a person, redirects to the people page after adding */
+
+  router.post('/', function(req, res){
+      console.log(req.body.house)
+      console.log(req.body)
+      var mysql = req.app.get('mysql');
+      var sql = "INSERT INTO students (fname, lname, type, class_year, house) VALUES (?,?,?,?,?)";
+      var inserts = [req.body.fname, req.body.lname, req.body.type, req.body.class_year, req.body.house];
+      sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+          if(error){
+              console.log(JSON.stringify(error))
+              res.write(JSON.stringify(error));
+              res.end();
+          }else{
+              res.redirect('/people');
+          }
+      });
+  });
+
+  /* The URI that update data is sent to in order to update a person */
+
+  router.put('/:id', function(req, res){
+      var mysql = req.app.get('mysql');
+      console.log(req.body)
+      console.log(req.params.id)
+      var sql = "UPDATE students SET fname=?, lname=?, type=?, class_year=?, house=? WHERE id=?";
+      var inserts = [req.body.fname, req.body.lname, req.body.house, req.body.class_year, req.params.id];
+      sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+          if(error){
+              console.log(error)
+              res.write(JSON.stringify(error));
+              res.end();
+          }else{
+              res.status(200);
+              res.end();
+          }
+      });
+  });
+
+  /* Route to delete a person, simply returns a 202 upon success. Ajax will handle this. */
+
+  router.delete('/:id', function(req, res){
+      var mysql = req.app.get('mysql');
+      var sql = "DELETE FROM students WHERE id = ?";
+      var inserts = [req.params.id];
+      sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+          if(error){
+              console.log(error)
+              res.write(JSON.stringify(error));
+              res.status(400);
+              res.end();
+          }else{
+              res.status(202).end();
+          }
+      })
   })
 
   return router;
